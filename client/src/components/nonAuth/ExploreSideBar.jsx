d2560de6e1457f1955862c25d875816104d41e02
Wirkus Tomasz <wirkus919@gmail.com>
2020-07-24 18:51:31 +0200

    Created basic for registration modal

--- client/src/components/nonAuth/ExploreSideBar.jsx
+++ client/src/components/nonAuth/ExploreSideBar.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 import Input from '../smallParts/Input';
 import SideBarImg from '../../img/twitter_login_sidebar_illustration.png';
+import RegisterModal from './RegisterModal';
 
 const ExploreSideBar = () => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
   const onChange = (e) => {
     setFormData({
@@ -21,11 +23,14 @@ const ExploreSideBar = () => {
     console.log('click');
   };
 
+  //modalfunc
+  function openModal(e) {
+    e.preventDefault();
+    setIsOpen(true);
+  }
+
   return (
-    <form
-      onSubmit={(e) => handleLogin(e)}
-      className='nonAuth__content__sidebar'
-    >
+    <form className='nonAuth__content__sidebar'>
       <img
         src={SideBarImg}
         className='nonAuth__content__sidebar__img'
@@ -52,13 +57,17 @@ const ExploreSideBar = () => {
         Nie pamiętasz hasła?
       </Link>
       <button
+        onClick={(e) => handleLogin(e)}
         disabled={!formData.email || !formData.password ? true : false}
         className='btn btn--dark btn--wide'
       >
         Zaloguj się
       </button>
       <p className='nonAuth__content__sidebar__p'>lub</p>
-      <button className='btn btn--wide'>Zarejestruj się</button>
+      <button onClick={openModal} className='btn btn--wide'>
+        Zarejestruj się
+      </button>
+      <RegisterModal modalIsOpen={modalIsOpen} />
     </form>
   );
 };
