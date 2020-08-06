94431432fa86cb455f7bceea6f2d561bd4c9d579
Wirkus Tomasz <wirkus919@gmail.com>
2020-08-06 15:04:04 +0200

    Added user box in navBar with logout funcionality

--- client/src/components/auth/ExploreAuth.jsx
+++ client/src/components/auth/ExploreAuth.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/auth';
 
 import NavBar from './NavBar';
 import MainContent from './mainContent/MainContent';
 import Recommended from './Recommended';
 
-const ExploreAuth = () => {
+const ExploreAuth = ({ user, logout }) => {
   return (
     <section className='auth'>
-      <NavBar />
+      <NavBar user={user} logout={logout} />
       <MainContent />
       <Recommended />
     </section>
   );
 };
 
-export default ExploreAuth;
+ExploreAuth.propTypes = {
+  logout: PropTypes.func.isRequired,
+};
+const mapStateToProps = (state) => ({
+  user: state.auth.user,
+});
+
+export default connect(mapStateToProps, { logout })(ExploreAuth);
