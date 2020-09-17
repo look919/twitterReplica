318626041024d86eac91b05f3b539e837ad75a8e
Wirkus Tomasz <wirkus919@gmail.com>
2020-09-17 18:00:59 +0200

    Removed top-margins from emoji&gif pickers in mobile view

--- client/src/components/auth/sidebars/NavBar.jsx
+++ client/src/components/auth/sidebars/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import Modal from 'react-modal';
 import CreateTweet from '../mainContent/tweet/CreateTweet';
